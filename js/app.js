65bc1d84ad3fbc97ccac11b9ede0ff000161cb3a
wanasak <u510610433@gmail.com>
2015-09-29 17:06:47 +0700

    add

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 (function() {
 	'use strict';
 
-	var app = angular.module('application', ['ui.router']);
+	var app = angular.module('application', ['ui.router'])
+		.config(function($stateProvider, $urlRouterProvider) {
+			$stateProvider
+				.state('index', {
+					url: '/',
+					template: 'index.html'
+				});
+		});
 
 	app.controller('mainController', function() {
 
